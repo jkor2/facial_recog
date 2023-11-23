d9310f9b17dade0fa503608ef5484f106bf80427
Jake Korobellis <jkorobellis@gmail.com>
2023-11-23 13:43:28 -0500

    Round face test

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Main:
         self._args = None
         self._face_cascade = None
         self._landmark_detector = None
-        self._image = "faces/oval.png"
+        self._image = "faces/round_test_11.png"
 
     def run_detection_stillshot(self):
         """
